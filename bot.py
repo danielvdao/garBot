e8ee98b51aeadcba0e6489ebc8a6bb03b942e5d7
Daniel Dao <daniel.vu.dao@gmail.com>
2016-09-17 18:37:27 -0500

    more changes to bot

--- bot.py
+++ bot.py
@@ -8,18 +8,10 @@ g = giphypop.Giphy()
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-@client.event
 async def on_message(message):
     if message.content.startswith('!lucky'):
-        counter = 0
-        searchResults = g.search(message.content.split('!lucky ')[1])
-        if not imgSearch:
+        searchResults = [x for x in g.search(message.content.split('!lucky ')[1])]
+        if not searchResults:
             await client.send_message(message.channel, 'There is no image to be found')
         else:
             img = searchResults[0].media_url
